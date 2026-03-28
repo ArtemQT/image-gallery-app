682bce0f504ab20432dd718fc926463fb4e4aa7e
Artem Shimanchuk <shimart0606@mail.ru>
2026-03-28 14:16:49 +0300

    chore: setup project architecture and aliases

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+
+	return (
+		<>
+			Basic module architecture
+		</>
+	)
+}
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@api': path.resolve(__dirname, './src/shared/api'),
+      '@components': path.resolve(__dirname, './src/shared/components'),
+      '@hooks': path.resolve(__dirname, './src/shared/hooks'),
+      '@styles': path.resolve(__dirname, './src/shared/styles'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    }
+  }
 })
