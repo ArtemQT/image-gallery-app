578ce13f5698d6cd9c4d5f0ff475fdfac6e0c9a6
Artem Shimanchuk <shimart0606@mail.ru>
2026-03-29 12:10:38 +0300

    feat: create mark up for header

--- src/layouts/root-layout/componets/header/header.tsx
+++ src/layouts/root-layout/componets/header/header.tsx
@@ -1,3 +1,35 @@
+import styles from './header.module.scss';
+import { Link, NavLink } from 'react-router-dom';
+import logoImg from '@assets/icons/logo.svg';
+import { ReactSVG } from 'react-svg';
+import { links } from '@shared/utils/header-links-list.ts';
+
 export const Header = () => {
-    return <header>header</header>;
+    return (
+        <header className={styles.header}>
+            <div className={`${styles.headerContainer} header-container`}>
+                <Link to="/">
+                    <img src={logoImg} alt="modsen-gallery-logo" width="287" height="49" />
+                </Link>
+                <nav className={styles.navigation}>
+                    <ul className={styles.navigationList}>
+                        {links.map((link) => (
+                            <li key={link.text} className={styles.navigationItem}>
+                                <NavLink to={link.to} className={styles.navigationLink}>
+                                    {({ isActive }) => (
+                                        <>
+                                            <ReactSVG src={link.iconSrc} className={styles.navigationItemIcon} data-is-active={isActive} />
+                                            <span className={styles.navigationItemText} data-is-active={isActive}>
+                                                {link.text}
+                                            </span>
+                                        </>
+                                    )}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
 };
