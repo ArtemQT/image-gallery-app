e688d487b9830d4ec5ff99cfaf65c913c83ba6eb
Artem Shimanchuk <shimart0606@mail.ru>
2026-03-31 18:45:22 +0300

    feat: make adaptive 404-page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,3 +1,16 @@
+import styles from './not-found-page.module.scss';
+
 export const NotFoundPage = () => {
-    return <h1>Error 404</h1>;
+    return (
+        <section className={styles.notFoundSection}>
+            <div className={styles.errorWrapper}>
+                <span className={styles.errorCode}>404</span>
+                <span className={styles.errorDescription}>NOT FOUND</span>
+            </div>
+            <h2 className={styles.notFoundTitle}>
+                The page was <span>not found</span>, please return to the main
+                page.
+            </h2>
+        </section>
+    );
 };
